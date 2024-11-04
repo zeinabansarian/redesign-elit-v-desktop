25944c24ee849c035ad6d90c7a4fdf031e955651
Zeinab <zeinabansarian6280@gmail.com>
2024-11-04 11:20:26 +0330

    complete galleryPage

--- js/galleryPage.js
+++ js/galleryPage.js
@@ -35,40 +35,98 @@ function raf(time) {
 
 requestAnimationFrame(raf)
 
+let toTop = document.querySelectorAll(".toTop")
 
-
-$('.gallery').imagesLoaded( function() {
-    // images have loaded
-    var imgbg = document.querySelector('.scaleContainer .imgS');
-    var scrollbarGallery = document.querySelector('.scaleContainer .scrollbarGallery span');
-    console.log(scrollbarGallery.style.width);
-    imgbg.style.clipPath = `inset( 99px 99px)`;
-    
-    gsap.from(".scaleContainer img",{
+toTop.forEach(element => {
+    gsap.from(element,{
         scrollTrigger:{
-            trigger:".scaleContainer",
-            start:"top top",
-            end:"top -100%",
-            pin:true,
-            scrub:1,
-       
-            onUpdate: self => {
-                imgbg.style.clipPath = `inset( ${(1 - self.progress)*100}px ${(1 - self.progress)*100}px)`;
-                // scrollbarGallery.style.width=`${(self.progress)*100}$`
-                scrollbarGallery.style.width=`${(self.progress)*100}%`;
-            }
+           trigger: element,
+            start:"top 80%",
+            end:"bottom bottom",
+            // markers:true,
+            
         },
-         scale:0.8,
+        opacity:0,
+        duration:1,
+        y:"20vh"
+
+    })
+});
+
+
+
+const serviceSlider = new Swiper('.serviceSlider', {
+   freeMode:true,
+   slidesPerView:3,
+   speed:3000,
+//    grabCursor:true,
+   spaceBetween:53,
+  });
+
+
+
+
+
+  let closePop = document.querySelector(".closePop")
+
+let openGallery = document.querySelectorAll(".openGallery")
+let zoomIn = document.querySelector(".zoomIn")
+let rotateRight = document.querySelector(".rotateRight")
+let rotateLeft = document.querySelector(".rotateLeft")
+let zoomOut = document.querySelector(".zoomOut")
+let closeSlider = document.querySelector(".closeSlider")
+let galleryFlag=true;
+let zoomMin =0.5;
+let zoomMax =1.7;
+let currZoom = 1;
+let curRotate = 0;
+let sGalleryFlag = true;
+
+  let imagesThumb = document.querySelectorAll(".scrollContainer .imgS")
+let popUpGalleryContainer = document.querySelector(".popUpGalleryContainer")
+let sGallery = document.querySelector(".sGallery")
+  imagesThumb.forEach(element => {
+    element.addEventListener("click" , function (params) {
+        let imgS = element.querySelector("img").getAttribute("src")
+        popUpGalleryContainer.classList.add("popOpen")
+        sGallery.querySelector(".imgS img").setAttribute("src",imgS)
+        
+        sGallery.classList.add("popOpen")
     })
   });
+
+
   
+zoomIn.addEventListener("click" , function (params) {
+    let currImg = document.querySelector(".popUpGalleryContainer .sGallery img");
+    console.log("zoomin");
+    if (currZoom<zoomMax) {
+        currZoom+=0.1
+        currImg.style.scale = currZoom
+    }
+})
+zoomOut.addEventListener("click" , function (params) {
+    let currImg = document.querySelector(".popUpGalleryContainer .sGallery img");
+    console.log("zoomout");
+    if (currZoom>zoomMin) {
+        currZoom-=0.1;
+        currImg.style.scale = currZoom
+    }
+})
+rotateLeft.addEventListener("click" , function (params) {
+    let currImg = document.querySelector(".popUpGalleryContainer .sGallery img");
+    curRotate+=90
+    currImg.style.rotate = `${-curRotate}deg`
+})
+rotateRight.addEventListener("click" , function (params) {
+    let currImg = document.querySelector(".popUpGalleryContainer .sGallery img");
+    curRotate+=90
+    currImg.style.rotate = `${curRotate}deg`
+})
+
+closeSlider.addEventListener("click", function (params) {
+    popUpGalleryContainer.classList.remove("popOpen")
+    sGallery.classList.remove("popOpen")
 
 
-
-const servSlider = new Swiper('.servSlider', {
-   freeMode:true,
-   slidesPerView:4,
-   speed:3000,
-   grabCursor:true,
-   spaceBetween:36,
-  });
+})
