2a7e1fe6537da6e9646391bc80e62b836797daa3
Zeinab <zeinabansarian6280@gmail.com>
2024-12-25 17:48:19 +0330

    change home page

--- js/galleryPage.js
+++ js/galleryPage.js
@@ -7,9 +7,10 @@ parallaxLeft.to('.topS' , {
 ScrollTrigger.create({
     animation:parallaxLeft , 
     trigger : ".scrollContainer",
-    start:"top 20%",
+    start:"top 30%",
     end:"bottom -200vw",
     scrub : 1,
+    // markers:true,
     
 })
 let parallaxRight = gsap.timeline();
@@ -19,9 +20,10 @@ parallaxRight.to('.btmS' , {
 ScrollTrigger.create({
     animation:parallaxRight , 
     trigger : ".scrollContainer",
-    start:"top 20%",
+    start:"top 30%",
     end:"bottom -200vw",
     scrub : 1,
+    // markers:true,
 
 })
 
@@ -67,73 +69,6 @@ const serviceSlider = new Swiper('.serviceSlider', {
 
 
 
-//   let closePop = document.querySelector(".closePop")
-
-// let openGallery = document.querySelectorAll(".openGallery")
-// let zoomIn = document.querySelector(".zoomIn")
-// let rotateRight = document.querySelector(".rotateRight")
-// let rotateLeft = document.querySelector(".rotateLeft")
-// let zoomOut = document.querySelector(".zoomOut")
-// let closeSlider = document.querySelector(".closeSlider")
-// let galleryFlag=true;
-// let zoomMin =0.5;
-// let zoomMax =1.7;
-// let currZoom = 1;
-// let curRotate = 0;
-// let sGalleryFlag = true;
-
-//   let imagesThumb = document.querySelectorAll(".scrollContainer .imgS")
-// let popUpGalleryContainer = document.querySelector(".popUpGalleryContainer")
-// let sGallery = document.querySelector(".sGallery")
-//   imagesThumb.forEach(element => {
-//     element.addEventListener("click" , function (params) {
-//         let imgS = element.querySelector("img").getAttribute("src")
-//         popUpGalleryContainer.classList.add("popOpen")
-//         sGallery.querySelector(".imgS img").setAttribute("src",imgS)
-        
-//         sGallery.classList.add("popOpen")
-//     })
-//   });
-
-
-  
-// zoomIn.addEventListener("click" , function (params) {
-//     let currImg = document.querySelector(".popUpGalleryContainer .sGallery img");
-//     console.log("zoomin");
-//     if (currZoom<zoomMax) {
-//         currZoom+=0.1
-//         currImg.style.scale = currZoom
-//     }
-// })
-// zoomOut.addEventListener("click" , function (params) {
-//     let currImg = document.querySelector(".popUpGalleryContainer .sGallery img");
-//     console.log("zoomout");
-//     if (currZoom>zoomMin) {
-//         currZoom-=0.1;
-//         currImg.style.scale = currZoom
-//     }
-// })
-// rotateLeft.addEventListener("click" , function (params) {
-//     let currImg = document.querySelector(".popUpGalleryContainer .sGallery img");
-//     curRotate+=90
-//     currImg.style.rotate = `${-curRotate}deg`
-// })
-// rotateRight.addEventListener("click" , function (params) {
-//     let currImg = document.querySelector(".popUpGalleryContainer .sGallery img");
-//     curRotate+=90
-//     currImg.style.rotate = `${curRotate}deg`
-// })
-
-// closeSlider.addEventListener("click", function (params) {
-//     popUpGalleryContainer.classList.remove("popOpen")
-//     sGallery.classList.remove("popOpen")
-
-
-// })
-
-
-
-
 jQuery('.openPara').readmore({
     speed: 100,
     collapsedHeight:45,
@@ -147,3 +82,5 @@ jQuery('.openPara').readmore({
         // }
       },
 });
+
+    
--- js/home.js
+++ js/home.js
@@ -1,16 +1,62 @@
 let section2 = document.querySelectorAll(".section2")
 $(function() {
     fullpage();
-  
+  setTimeout(() => {
+    gsap.registerPlugin(ScrollTrigger);
+
+    let parallaxLeft = gsap.timeline();
+    parallaxLeft.to('.topS' , {
+        x : "20vw"
+    })
+    ScrollTrigger.create({
+        animation:parallaxLeft , 
+        trigger : ".scrollContainer",
+        start:"top 20%",
+        end:"bottom -200vw",
+        scrub : 1,
+        scroller: ".section3 .fp-overflow",
+        
+      })
+      let parallaxRight = gsap.timeline();
+      parallaxRight.to('.btmS' , {
+        x : "-20vw"
+      })
+      ScrollTrigger.create({
+        animation:parallaxRight , 
+        trigger : ".scrollContainer",
+        start:"top 20%",
+        end:"bottom -200vw",
+        scrub : 1,
+        scroller: ".section3 .fp-overflow",
+    
+    
+    })
+    
+    
+    gsap.to(".gallery .section1",{
+      scrollTrigger:{
+         trigger:".gallery",
+         start:"top 10%",
+         end:"bottom bottom",
+        //  onEnter: () => document.querySelector("header").classList.add("color"),
+         
+        //  onLeaveBack: () => document.querySelector("header").classList.remove("color")
+        scroller: ".section3 .fp-overflow",
+    
+      }
+     })
+  }, 100);
   });
 // FULLPAGE
 function fullpage(){
   $('#fullpage').fullpage({
     //   scrollingSpeed: 1000,
-      scrollBar: true,
+      // scrollBar: true,
     //   autoScrolling: true,
 				//scrollOverflow:true,
 				// fitToSection: false,
+        scrollOverflow: true,
+        scrolloverflowmacstyle: false,
       afterLoad: function(origin, destination, direction, trigger){
         $(".section2").removeClass("activeImg")
         if (destination>2) {
@@ -59,44 +105,3 @@ const slider1 = new Swiper('.slider1', {
   });
 
 
-  gsap.registerPlugin(ScrollTrigger);
-
-let parallaxLeft = gsap.timeline();
-parallaxLeft.to('.topS' , {
-    x : "20vw"
-})
-ScrollTrigger.create({
-    animation:parallaxLeft , 
-    trigger : ".scrollContainer",
-    start:"top 20%",
-    end:"bottom -200vw",
-    scrub : 1,
-    
-    
-  })
-  let parallaxRight = gsap.timeline();
-  parallaxRight.to('.btmS' , {
-    x : "-20vw"
-  })
-  ScrollTrigger.create({
-    animation:parallaxRight , 
-    trigger : ".scrollContainer",
-    start:"top 20%",
-    end:"bottom -200vw",
-    scrub : 1,
-    
-
-})
-
-
-gsap.to(".gallery .section1",{
-  scrollTrigger:{
-     trigger:".gallery",
-     start:"top 10%",
-     end:"bottom bottom",
-    //  onEnter: () => document.querySelector("header").classList.add("color"),
-     
-    //  onLeaveBack: () => document.querySelector("header").classList.remove("color")
-
-  }
- })
